9b5b13e1626ceea9970292b79753c7ddff8ff4d8
Tommaso Ripanti <tommaso.rip03@gmail.com>
2026-01-11 10:30:08 +0100

    Reapply "Merge branch 'main' of https://github.com/Gianno01/RV_2025-2026"
    This reverts commit c89c6194ba403c7ed1cbd7ed8b2a1ee4f217402f.

--- RV25_26/Assets/Scripts/InteractionSystem/AnimateItem.cs
+++ RV25_26/Assets/Scripts/InteractionSystem/AnimateItem.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
-public class TestItem : MonoBehaviour, IsInteractable 
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class AnimateItem : MonoBehaviour, IsInteractable 
 {
+    [SerializeField] private PlayableAsset[] timelines;
+    private bool _toggleState = false;
+    private PlayableDirector _director;
+
+    void Start() 
+    {
+        _director = GetComponent<PlayableDirector>();
+    }
     public void Interact() {
-        Debug.Log("Ho interagito con l'oggetto!");
+        //Debug.Log("Ho interagito con l'oggetto!");
         // Qui metterai il codice per raccogliere/aprire
-        Destroy(gameObject); // Esempio: distrugge l'oggetto
+        //Destroy(gameObject); // Esempio: distrugge l'oggetto
+        _toggleState = !_toggleState;
+        _director.Play(timelines[_toggleState ? 1 : 0]);
     }
     public string GetDescription() { return "Oggetto di prova"; }
 }
