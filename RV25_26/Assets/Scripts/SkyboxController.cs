09e7e89d78f2bab7e63e27b8fdb24b17a94697ff
Tommaso Ripanti <tommaso.rip03@gmail.com>
2026-02-14 12:52:23 +0100

    no more sun rotation

--- RV25_26/Assets/Scripts/SkyboxController.cs
+++ RV25_26/Assets/Scripts/SkyboxController.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class SkyboxController : MonoBehaviour
 {
     [SerializeField] private float _skyboxSpeed = 1f;
-    [SerializeField] private float _sunSpeed = 1f;
+    //[SerializeField] private float _sunSpeed = 1f;
     [SerializeField] private Transform _directionalLight;
     void Update() {
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * _skyboxSpeed);
-        _directionalLight.Rotate(Vector3.up, Time.deltaTime * _sunSpeed, Space.World);
+        //_directionalLight.Rotate(Vector3.up, Time.deltaTime * _sunSpeed, Space.World);
         DynamicGI.UpdateEnvironment(); 
     }
 }
